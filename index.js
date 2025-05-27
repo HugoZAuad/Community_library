67c8715b2097666ab6c23fa3a2767f238c6fb9d5
HugoZAuad <hugozeymer@hotmail.com>
2025-05-27 18:39:07 -0300

    Feat: service, controller and routes users

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 import express from "express";
+import userRouters from "./src/routes/user.routes.js";
 const app = express();
 
 app.use(express.json());
+app.use(userRouters);
 
-const users = [];
 
-//METHOD - GET, POST, PUT/PATCH, DELETE
-
-//EXPLICAÇÃO DOS METODOS HTTP
-//METHOD - GET - PARA LISTAR
-//METHOD - POST - PARA CRIAR
-//METHOD - PUT/PATCH - PARA ATUALIZAR
-//METHOD - DELTE - PARA DELETAR
-
-//NAME - /SEMPRE NO PLURAL
-
-//CALLBACK FUNCTION - (req, res) => ONDE EXCECUTAMOS O BACKEND (LOGICA E REGRAS DE NEGOCIO)
-
-app.post("/users", (req, res) => {
-  const body = req.body;
-  users.push(body);
-  res.status(201).send("User created successfully");
-});
-
-app.get("/users", (req, res) => {
-    res.json({users});
-})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
